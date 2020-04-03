5334bac7c266644b58747840d6404dec49ca69e7
w1032593545 <1032593545@qq.com>
2020-04-03 09:49:45 +0800

    数据库增删改查

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,10 +1,67 @@
-from flask import Flask
+import os
+import sys
 
+from flask import Flask, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+import click
 
+WIN = sys.platform.startswith("win")
+if WIN:
+    prefix = "sqlite:///"  # windows平台
+else:
+    prefix = "sqlite:////"  # Mac，linux
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = prefix + \
+    os.path.join(app.root_path, 'data.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-@app.route('/<name>')
-def index(name):
-    return "%s"%name
 
+# 数据层
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+@app.route('/')
+def index():
+    user = User.query.first()
+    movie = Movie.query.all()
+    return render_template("index.html", user=user, movies=movie)
+
+
+# 自定义命令
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help="先删除在创建")
+def initdb(drop):
+    if drop:
+        db.create_all
+    db.create_all()
+    click.echo("初始化数据库完成")
+
+
+# 向数据库中插入数据
+@app.cli.command()
+def forge():
+    name = "李白"
+    m = [
+            {"title": "萨霍", "year": "2019"},
+            {"title": "星游记", "year": "2020"},
+            {"title": "急速备战", "year": "2019"},
+            {"title": "叶问4", "year": "2019"},
+            {"title": "三人行", "year": "2016"},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for i in m:
+        movie = Movie(title=i["title"], year=i["year"])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo("导入数据完成")
