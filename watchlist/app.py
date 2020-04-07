2d5a49935f526a3e04add5d8fc4a3542dcad89b9
w1032593545 <1032593545@qq.com>
2020-04-07 14:51:23 +0800

    表单操作

--- watchlist/app.py
+++ watchlist/app.py
@@ -53,6 +53,34 @@ def index():
     return render_template("index.html", movies=movie)
 
 
+# 更新
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == "POST":
+        title = request.form["title"]
+        year = request.form['year']
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('输入有误')
+            return redirect(url_for('edit',movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('电影更新完成')
+        return redirect(url_for('index'))
+    return render_template("edit.html",movie=movie)
+
+
+# 删除
+@app.route('/movie/delete/<int:movie_id>',methods=['GET','POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('删除完成')
+    return redirect(url_for('index'))
+
+
 # 自定义命令
 @app.cli.command()  # 注册为命令
 @click.option('--drop', is_flag=True, help="先删除在创建")
