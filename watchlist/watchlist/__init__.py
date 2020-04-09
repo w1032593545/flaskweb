ba709c30b062c655893e678ff9b480993cdfcd12
w1032593545 <1032593545@qq.com>
2020-04-09 10:12:33 +0800

    购物车hcjjb

--- watchlist/watchlist/__init__.py
+++ watchlist/watchlist/__init__.py
@@ -13,7 +13,7 @@ else:
     prefix = "sqlite:////"  # Mac，linux
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE','data.db'))
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY','dev')
 
