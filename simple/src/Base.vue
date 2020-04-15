61c24301384fae6aa1e7bfc680fef7feffc40530
w1032593545 <1032593545@qq.com>
2020-04-15 16:00:42 +0800

    index.vue

--- simple/src/Base.vue
+++ simple/src/Base.vue
@@ -28,8 +28,8 @@ export default {
 }
 </script>
 
-<style scoped>
-html,body,div,span,applet,object,,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video {
+<style>
+html,body,div,span,applet,object,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video {
 	margin:0;
 	padding:0;
 	border:0;
@@ -73,6 +73,11 @@ input:focus,select:focus,textarea:focus {
 	outline:-webkit-focus-ring-color auto 0;
 }
 /* Base样式 */
+body {
+  background: #f0f2f5;
+  font-size: 14px;
+  color:#444;
+}
 .app-head {
   background: #363636;
   color:#b2b2b2;
